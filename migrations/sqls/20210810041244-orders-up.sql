ce0e45d8d522f40f4f7dbb773aeff3f365f7dc58
Bernabe Nieto Jr <dos_nieto@yahoo.com>
2021-08-10 17:34:57 -0500

    first commit

---
+++ migrations/sqls/20210810041244-orders-up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE orders(id SERIAL PRIMARY KEY, prod_id INTEGER, quantity REAL, user_id INTEGER, status VARCHAR(100));
---
+++ src/models/product.ts
@@ -0,0 +1,67 @@
+import client from "../database";
+
+export type Product {
+    id: Number;
+    name: string;
+    price: Number;
+    category: string;
+}
+
+export class StoreProduct {
+
+    async index(): Promise<Product[]> {
+        
+        try {
+
+            const conn = await client.connect();
+            const sql = 'SELECT * FROM product';
+            const result = await  conn.query(sql);
+
+            conn.release();
+
+            return result.rows;
+
+        }
+        catch(err) {
+            throw new Error(`Cannot get products ${err}`);
+        }
+    }
+
+    async show(id: string): Promise<Product> {
+
+        try {
+            const sql = 'SELECT * FROM product WHERE id =($1)';
+
+            const conn = await client.connect();
+            const result = await conn.query(sql, [id]);
+
+            conn.release();
+
+            return result.rows[0];
+        
+        }catch (err) {
+            throw new Error(`Could not find book ${id}. Error: ${err}`);
+        }
+
+    }
+
+    async create(prod: Product): Promise<Product> {
+        try {
+            const sql = 'INSERT INTO product (id, name, price, category) VALUES($1, $2, $3, $4) RETURNING *';
+
+            const conn = await client.connect();
+
+            const result = await conn.query(sql, [prod.id, prod.name, prod.price, prod.category]);
+
+            const product = result.rows[0];
+
+            conn.release();
+            
+            return product;
+
+        } catch (err) {
+            throw new Error(`Could not add new product ${prod.name}. Error ${err}`);
+        }
+    }
+
+}
